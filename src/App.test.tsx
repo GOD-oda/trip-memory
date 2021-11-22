619fe6b7465f0e35687b6b64cad40977602e1ef7
t.oda <takahiro.tech.oda@gmail.com>
2021-11-22 15:56:39 +0900

    Typescript

---
+++ src/App.test.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+test('renders learn react link', () => {
+  expect('foo').toBe('foo')
+
+  // render(<App />);
+  // const linkElement = screen.getByText(/top page/i);
+  // expect(linkElement).toBeInTheDocument();
+});
